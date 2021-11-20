9f78dafbe6c731346de47d2f92eeeb1833307bda
Soraya <soraya.l.g.88@gmail.com>
2021-11-20 15:16:38 +0100

    Dbz mainPage show list of characters and add new character

--- ng-practice/src/app/components/dbz/main-page/main-page.component.ts
+++ ng-practice/src/app/components/dbz/main-page/main-page.component.ts
@@ -10,12 +10,32 @@ interface Character {
 })
 export class MainPageComponent {
 
+  characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 15000
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    }
+  ]
+
   new:Character = {
-    name: 'Trucks',
-    power: 14000
+    name: '',
+    power: 0
   }
 
   add() {
+    if(this.new.name.trim().length === 0) {
+      //Exits the method
+      return;
+    }
     console.log(this.new)
+    this.characters.push(this.new);
+    this.new = {
+      name: '',
+      power:0
+    }
   }
 }
