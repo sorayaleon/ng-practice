095a7f9bd82565d364fca7b76fb78755ebcada0c
Soraya <soraya.l.g.88@gmail.com>
2021-11-20 15:07:17 +0100

    Dbz mainPage ngModule

--- ng-practice/src/app/components/dbz/main-page/main-page.component.ts
+++ ng-practice/src/app/components/dbz/main-page/main-page.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  new:Character = {
+    name: 'Trucks',
+    power: 14000
+  }
+
   add() {
-    console.log("Hola")
+    console.log(this.new)
   }
 }
